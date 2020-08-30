e1e19618846c13b5bc3309fd8de42573b32ec73c
culturadevops <culturadevops@gmail.com>
2020-08-30 14:45:24 -0500

    arreglando import

--- exe/exe.go
+++ exe/exe.go
@@ -4,10 +4,9 @@ import (
 	"bytes"
 	"fmt"
 	"os/exec"
-
 )
 
-func Run(cmd *exec.Cmd,flagStdOut bool) string{
+func Run(cmd *exec.Cmd, flagStdOut bool) string {
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
@@ -16,16 +15,16 @@ func Run(cmd *exec.Cmd,flagStdOut bool) string{
 		fmt.Printf("cmd.Run() failed with %s\n", err)
 	}
 	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
-	if (flagStdOut){
+	if flagStdOut {
 		fmt.Printf("out:\n%s\n", outStr)
 	}
 	fmt.Printf("err:\n%s\n", errStr)
 	return outStr
 }
 
-func RunWithData(cmd *exec.Cmd,data string) {
+func RunWithData(cmd *exec.Cmd, data string) {
 	buffer := bytes.Buffer{}
 	buffer.Write([]byte(data))
 	cmd.Stdin = &buffer
-	Run(cmd,false)
+	Run(cmd, false)
 }
