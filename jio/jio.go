75feb829b2d6d94a383b5dd295e5fd910b7cde78
culturadevops <culturadevops@gmail.com>
2020-10-03 10:27:24 -0500

    subinedo cambio en jio

--- jio/jio.go
+++ jio/jio.go
@@ -16,7 +16,7 @@ func IsFileExist(dir string) bool {
 	}
 	return false
 }
-func CreateDir(directorio string) {
+func CreateFolder(directorio string) {
 
 	if _, err := os.Stat(directorio); os.IsNotExist(err) {
 		err = os.Mkdir(directorio, 0755)
